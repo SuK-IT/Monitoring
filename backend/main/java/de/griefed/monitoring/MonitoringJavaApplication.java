e25333ae9cccfde74839ba806125bed98dca13d7
Griefed <griefed@griefed.de>
2021-12-01 15:29:20 +0100

    Initial commit

---
+++ backend/main/java/de/griefed/monitoring/MonitoringJavaApplication.java
@@ -0,0 +1,13 @@
+package de.griefed.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringJavaApplication.class, args);
+    }
+
+}
